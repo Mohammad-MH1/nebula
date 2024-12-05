fad040a339394021770751048d3671550074c083
Mohammad Mohammadi <Mohammadyahoo15@gmail.com>
2024-12-05 13:20:54 +0330

    Add event handler to sort section

--- src/components/ProductsHeader/ProductsHeader.tsx
+++ src/components/ProductsHeader/ProductsHeader.tsx
@@ -1,15 +1,18 @@
+import { useFiltersContext } from '../../contexts/FiltersContext';
 import styles from './ProductsHeader.module.css';
 
 function ProductsHeader() {
+  const { state, dispatch } = useFiltersContext();
   return (
     <div className={styles.productsHeader}>
       <p>X number of products</p>
-      <select>
-        <option value=''>Sort by Default</option>
-        <option value=''></option>
-        <option value=''></option>
-        <option value=''> </option>
-        <option value=''></option>
+      <select
+        value={state.category}
+        onChange={e => dispatch({ type: 'SET_SORT', payload: e.target.value })}
+      >
+        <option value='default'>Sort by Default</option>
+        <option value='asc'>Lowest Price</option>
+        <option value='desc'>Highest Price</option>
       </select>
     </div>
   );
